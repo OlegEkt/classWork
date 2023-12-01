b9fb8f9dd6ad9e9f74e56db4d0cd1abd2aae5b67
OlegEkt <109219534+OlegEkt@users.noreply.github.com>
2023-12-01 18:13:20 +0500

    Delete lesson_1 directory

--- lesson_1/firstApp/app_weather/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def app_weather(request, *args, **kwargs):
-     return HttpResponse(f'The weather is fine today!')
