f00c4f33f0f2d870aa50611286b7bb8fc1c1290b
OlegEkt <109219534+OlegEkt@users.noreply.github.com>
2023-12-01 11:56:28 +0500

    Delete lesson_3 directory

--- lesson_3/food/app_food/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppFoodConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_food'
--- lesson_3/food/app_food/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import products_list
-
-
-urlpatterns = [
-     path('catalog', products_list, name='catalog')
-]
