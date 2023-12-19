df295fae42b3c92d82082ef1a9381e31d88003c7
OlegEkt <scala37@yandex.ru>
2023-12-19 11:42:57 +0500

    new files

--- lesson_3/food/food/settings.py
+++ lesson_3/food/food/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app_food',
     'app_meal',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- lesson_3/food/food/urls.py
+++ lesson_3/food/food/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_food.urls')),
-    path('restaurant/', include('app_meal.urls'))
+    # path('', include('app_food.urls')),
+    # path('restaurant/', include('app_meal.urls')),
+    path('blog/', include('blog.urls'))
 ]
