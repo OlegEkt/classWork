213c5b7e57e96119faa96da96a5e3aee1694d9be
OlegEkt <scala37@yandex.ru>
2024-01-16 11:59:49 +0500

    n

--- lesson_4_auth/lesson_4_1/my_auth/urls.py
+++ lesson_4_auth/lesson_4_1/my_auth/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.conf.urls.static import static
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
-from .views import main_page, login_view, logout_view, MyLogoutView
+from django.urls import path
+from .views import main_page, login_view, logout_view, MyLogoutView, register_view, RecordFormView
+from django.conf import settings
+
+
 
 urlpatterns =[
     path('', main_page, name='main'),
     # path('login', login_view, name='login'),
     path('login/', LoginView.as_view(template_name='my_auth/login.html',
      redirect_authenticated_user=True), name='login'),
-    path('logout', MyLogoutView.as_view(), name='logout')
-]
+    path('logout', MyLogoutView.as_view(), name='logout'),
+    path('register', register_view, name='register'),
+    path('record_create/', RecordFormView.as_view(), name='record_create')
+    ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
